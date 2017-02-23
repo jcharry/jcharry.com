be975acba498084dc75bfe3264d04ed9ecc46874
jcharry <jamie.charry@gmail.com>
2017-02-22 23:22:34 -0500

    update ga

--- app/app.jsx
+++ app/app.jsx
@@ -14,15 +14,6 @@ var store = configure();
 import projects from 'app/db/projects';
 store.dispatch(initializeProjects(projects));
 
-// Google analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-89688645-1', 'auto');
-ga('send', 'pageview');
-// End Google Analytics
 
 // Main styles
 import './styles/main.scss';
